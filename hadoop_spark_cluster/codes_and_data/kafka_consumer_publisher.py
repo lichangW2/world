a71c6d0406e7d698b4bcf75a805b7cb4d608ca1c
lichangW <1208284072@qq.com>
2019-02-09 23:46:19 +0800

    update

---
+++ hadoop_spark_cluster/codes_and_data/kafka_consumer_publisher.py
@@ -0,0 +1,77 @@
+import os
+import sys
+import time
+import getopt
+import json
+import random
+import hashlib
+import string
+
+import traceback
+import multiprocessing as mp
+
+
+from kafka import KafkaConsumer
+from kafka import KafkaProducer
+
+LOG='{"time":%d,"uid":"%d","req":{"uri":"%s"}}'
+
+HELP="""
+kafka_consumer_publisher [options]
+  [options]:
+    -h[--help]: help info
+    -p[--producer]: start as the info producers 
+    -c[--consumer]: start as the consumers
+"""
+
+def KFProducer(proc_id,topic,server):
+    kp=KafkaProducer(bootstrap_servers=server, value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+                  compression_type='gzip')
+    sys.stdout.flush()
+    try:
+        while True:
+            print("here:",proc_id)
+            time.sleep(1)
+            kp.send(topic,LOG%(int(time.time()),proc_id,''.join(random.sample(string.ascii_letters+string.digits,10))))
+    except Exception as _e:
+        print("error: ",type(_e),_e)
+        print(traceback.format_exc())
+
+
+def KFConsumer(topic,group, server):
+    sys.stdout.flush()
+    print("start consumer... ")
+    consumer= KafkaConsumer(topic,group_id = group, bootstrap_servers=server,value_deserializer=lambda v: json.loads(v) )
+    for msg in consumer:
+        print("receive one message: {}\n".format(msg))
+
+
+if __name__=="__main__":
+
+    opts,_=getopt.getopt(sys.argv[1:],"-h-p-c",["help","producer","consumer"])
+    topic="test-kafka"
+    server=['localhost:9092']
+
+    try:
+        for opt_name, opt_args in opts:
+            print("opt_name %s, opt_args %s"%(opt_name, opt_args))
+            if opt_name in ("-h","--help"):
+                print(HELP)
+            elif opt_name in ("-p","--producer"):
+                # producer is processing safe
+                pool=mp.Pool(processes=3)
+                for id in xrange(3):
+                    print("id ",id)
+                    ret=pool.apply_async(KFProducer,args=(id,topic,server,))
+
+                print("3 producers is running...")
+                pool.close()
+                pool.join()
+            elif opt_name in ("-c","--consumer"):
+                KFConsumer(topic,"testgroup-id",server)
+            else:
+                print(HELP)
+            sys.exit(0)
+        print(HELP)
+    except Exception as _e:
+        print(traceback.format_exc())
--- hadoop_spark_cluster/hadoop_deploy/init_kafka.sh
+++ hadoop_spark_cluster/hadoop_deploy/init_kafka.sh
@@ -3,6 +3,6 @@ nohup /opt/kafka/kafka_2.11-2.1.0/bin/kafka-server-start.sh /opt/kafka/kafka_2.1
 /opt/kafka/kafka_2.11-2.1.0/bin/kafka-server-stop.sh
 
 ##创建topic, 发送消息，接收消息
-kafka-topics.sh --create --zookeeper localhost:2181 --topic test --replication-factor 1 --partitions 1
-kafka-console-producer.sh --broker-list localhost:9092 --topic test
-kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic test --from-beginning
+/opt/kafka/kafka_2.11-2.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --topic test --replication-factor 1 --partitions 1
+/opt/kafka/kafka_2.11-2.1.0/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test
+/opt/kafka/kafka_2.11-2.1.0/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic test --from-beginning
